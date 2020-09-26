2dcd954bd146fedd5a14e1703cb8d0147ba734ee
Brady-Malott <55215446+Brady-Malott@users.noreply.github.com>
2020-09-26 01:23:57 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Firebase SDK
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// React-Firebase hooks
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCLY6epj2T1jCDk_c9iO4JySrjvg8ctg6o",
+  authDomain: "covid-dashboard-e6fac.firebaseapp.com",
+  databaseURL: "https://covid-dashboard-e6fac.firebaseio.com",
+  projectId: "covid-dashboard-e6fac",
+  storageBucket: "covid-dashboard-e6fac.appspot.com",
+  messagingSenderId: "265499291442",
+  appId: "1:265499291442:web:495e39e7df2bc5f826712b",
+  measurementId: "G-37K20WH1G4"
+})
+
+// Reference SDK with global vars
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <main className="container App-main">
+        <p>Welcome to the site</p>
+      </main>
     </div>
   );
 }
