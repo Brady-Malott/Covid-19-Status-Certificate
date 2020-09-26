34033df332143d8c48603ee5b74f9402ecab38ae
Aryaman73 <aryaman.singh.2012@gmail.com>
2020-09-26 13:39:38 -0400

    cleaded app.js (again)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
-// import 'firebase/auth';
 import {auth, firestore, firebase} from './firebase.js';
 import CovidForm from './components/CovidForm';
 
@@ -19,24 +16,6 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
-
-// // Firebase SDK
-
-
-// firebase.initializeApp({
-//   apiKey: "AIzaSyCLY6epj2T1jCDk_c9iO4JySrjvg8ctg6o",
-//   authDomain: "covid-dashboard-e6fac.firebaseapp.com",
-//   databaseURL: "https://covid-dashboard-e6fac.firebaseio.com",
-//   projectId: "covid-dashboard-e6fac",
-//   storageBucket: "covid-dashboard-e6fac.appspot.com",
-//   messagingSenderId: "265499291442",
-//   appId: "1:265499291442:web:495e39e7df2bc5f826712b",
-//   measurementId: "G-37K20WH1G4"
-// })
-
-// // Reference SDK with global vars
-// const auth = firebase.auth();
-// const firestore = firebase.firestore();
 
 function App() {
 
