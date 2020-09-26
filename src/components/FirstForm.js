f61dd14650e314b7e796bc633a6c4f78f5582b0a
Brady-Malott <55215446+Brady-Malott@users.noreply.github.com>
2020-09-26 04:33:47 -0400

    finished first form template

--- src/components/FirstForm.js
+++ src/components/FirstForm.js
@@ -26,7 +26,7 @@ export default function FirstForm() {
             autoComplete="given-name"
           /> */}
           <InputLabel id="work-sector">Work Sector</InputLabel>
-          <Select labelId="work-sector" id="select" fullWidth>
+          <Select labelId="work-sector" id="work-select" fullWidth>
             <MenuItem value="1">Health care and social assistance</MenuItem>
             <MenuItem value="2">Educational services</MenuItem>
             <MenuItem value="3">Accommodation and food services</MenuItem>
@@ -48,63 +48,24 @@ export default function FirstForm() {
             type="number"
           />
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Address line 1"
-            fullWidth
-            autoComplete="shipping address-line1"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-          />
+        <Grid item xs={12} sm={6}>
+          <InputLabel id="gender">Gender</InputLabel>
+          <Select labelId="gender" id="gender-select" fullWidth>
+            <MenuItem value="1">Male</MenuItem>
+            <MenuItem value="2">Female</MenuItem>
+            <MenuItem value="3">Other</MenuItem>
+          </Select>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField id="state" name="state" label="State/Province/Region" fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
+          <InputLabel id="race">Race</InputLabel>
+          <Select labelId="race" id="race-select" fullWidth>
+            <MenuItem value="1">White</MenuItem>
+            <MenuItem value="2">Arab</MenuItem>
+            <MenuItem value="3">Filipino</MenuItem>
+            <MenuItem value="4">Chinese</MenuItem>
+            <MenuItem value="5">Black</MenuItem>
+            <MenuItem value="6">Black</MenuItem>
+          </Select>
         </Grid>
       </Grid>
     </React.Fragment>
