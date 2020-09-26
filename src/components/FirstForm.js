6b2db51359d744402524b7ca97ee56653f55a04a
Brady-Malott <55215446+Brady-Malott@users.noreply.github.com>
2020-09-26 04:10:05 -0400

    Added first drop down on First Form

--- src/components/FirstForm.js
+++ src/components/FirstForm.js
@@ -2,34 +2,50 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
 export default function FirstForm() {
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Shipping address
+        Demographic Info
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
-          <TextField
+          {/* <TextField
             required
-            id="firstName"
-            name="firstName"
+            id="work-sector"
+            name="work-sector"
             label="First name"
             fullWidth
             autoComplete="given-name"
-          />
+          /> */}
+          <InputLabel id="work-sector">Work Sector</InputLabel>
+          <Select labelId="work-sector" id="select" fullWidth>
+            <MenuItem value="1">Health care and social assistance</MenuItem>
+            <MenuItem value="2">Educational services</MenuItem>
+            <MenuItem value="3">Accommodation and food services</MenuItem>
+            <MenuItem value="4">Finance and insurance, real estate, rental, and leasing</MenuItem>
+            <MenuItem value="5">Manufacturing</MenuItem>
+            <MenuItem value="6">Wholesale trade, retail trade</MenuItem>
+            <MenuItem value="7">Professional, scientific, and technology services</MenuItem>
+            <MenuItem value="8">Other</MenuItem>
+          </Select>
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
             required
-            id="lastName"
-            name="lastName"
-            label="Last name"
+            id="age"
+            name="age"
+            label="Age"
             fullWidth
-            autoComplete="family-name"
+            autoComplete="Age"
+            type="number"
           />
         </Grid>
         <Grid item xs={12}>
